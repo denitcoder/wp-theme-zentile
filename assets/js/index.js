630d42f0c188373bf30b67a3931e717e80feda2b
Den <denitcoder@gmail.com>
2020-07-02 01:06:35 +0700

    Improve accessibility

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,16 @@ import { domReady } from "./utils";
 domReady(() => {
     mediumZoom('.wp-block-image > img');
 
+    // Let the document know when the keyboard is being used
+    document.body.addEventListener('mousedown', _ => {
+        document.body.classList.remove('focus-keyboard');
+    });
+    document.body.addEventListener('keydown', event => {
+        if (event.key === 'Tab') {
+            document.body.classList.add('focus-keyboard');
+        }
+    });
+
     // Mobile navigation
     const sidebar = document.querySelector('#site-sidebar');
 
--- assets/js/off-canvas.js
+++ assets/js/off-canvas.js
@@ -22,19 +22,62 @@ export class OffCanvas {
 
         this.showButton.addEventListener('click', this.show.bind(this));
         this.closeButton.addEventListener('click', this.close.bind(this));
-
         this.backdrop.addEventListener('click', event => {
             if (event.target === this.backdrop) {
                 this.close();
             }
         });
+
+        this.onKeyDownBinded = this.onKeyDown.bind(this);
+
+        const focusableElements = [ ...element.querySelectorAll('a, button, input, textarea, select, [tabindex]:not([tabindex="-1"])') ]
+            .filter(el => !el.hasAttribute('disabled'))
+
+        if (focusableElements.length) {
+            this.firstFocusableElement = focusableElements[0];
+            this.lastFocusableElement = focusableElements[focusableElements.length - 1];
+        }
     }
 
     show() {
+        document.body.style.paddingRight = `${window.innerWidth - document.body.scrollWidth}px`;
+        document.body.style.overflow = 'hidden';
+
         this.backdrop.classList.add(this.options.classes.open);
+        this.showButton.setAttribute('aria-expanded', true);
+        this.closeButton.focus();
+
+        document.addEventListener('keydown', this.onKeyDownBinded);
     }
 
     close() {
+        document.body.style.paddingRight = '0';
+        document.body.style.overflow = 'auto';
+
         this.backdrop.classList.remove(this.options.classes.open);
+        this.showButton.setAttribute('aria-expanded', false);
+        this.showButton.focus();
+
+        document.removeEventListener('keydown', this.onKeyDownBinded);
+    }
+
+    onKeyDown(event) {
+        if (event.key === 'Escape') {
+            this.close();
+            return;
+        }
+
+        // Trap focus inside the modal
+        if (event.key === 'Tab') {
+            if (!event.shiftKey && document.activeElement === this.lastFocusableElement) {
+                event.preventDefault();
+                this.firstFocusableElement.focus();
+            }
+    
+            if (event.shiftKey && document.activeElement === this.firstFocusableElement) {
+                event.preventDefault();
+                this.lastFocusableElement.focus();
+            }
+        }
     }
 }
