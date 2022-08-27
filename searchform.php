02b4fb50ca67de699cdc5ec61631dd768c920472
Den <denitcoder@gmail.com>
2022-08-27 13:24:48 +0700

    Improve search form accessibility, fixes #17

--- searchform.php
+++ searchform.php
@@ -2,8 +2,9 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
     <label for="<?php echo $unique_id; ?>">
+        <span class="screen-reader-text"><?php _e('Search for:', 'zentile'); ?></span>
         <input type="search" id="<?php echo $unique_id; ?>" class="search-form__input" placeholder="<?php echo esc_attr_x('Search', 'Placeholder', 'zentile'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-        
+
         <?php zentile_the_theme_svg('search'); ?>
     </label>
 </form>
